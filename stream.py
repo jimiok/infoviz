c67d74965e85a5eb866689e218484352d28d2ae8
Jimi Oksman <jimi.oksman@hotmail.com>
2024-12-13 14:24:28 +0000

    Fixed maps, more data

--- stream.py
+++ stream.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 import plotly.express as px
 
@@ -8,13 +9,16 @@ df = pd.read_pickle('excel_pickle.pkl')
 # Ensure the DataFrame has a 'country' column and a 'value' column to visualize
 # For this example, we'll assume you have 'Country' and 'Value' columns in your DataFrame
 
+st.set_page_config(layout="wide", page_icon=":potable_water:")
 st.title("Sustainable Development Goal (SDG) 6: Clean Water and Sanitation")
 
-number = st.slider('Select a year', min_value=2000, max_value=2022, value=2010)
+st.sidebar.title("Data selection")
+number = st.sidebar.slider('Select a year', min_value=2000, max_value=2022, value=2010)
 
-option = st.selectbox(
+option = st.sidebar.selectbox(
     "Choose an area:",
-    ("URBAN", "RURAL", "ALLAREA"),
+    options=["URBAN", "RURAL", "ALLAREA"],
+    format_func=lambda x: {"URBAN": "Urban Areas", "RURAL": "Rural Areas", "ALLAREA": "All Areas"}[x]
 )
 
 
@@ -26,7 +30,8 @@ fig = px.choropleth(df,
                     locationmode='country names',  # or 'ISO-3' if using 3-letter country codes
                     color='Value',  # The data column you want to visualize
                     hover_name='GeoAreaName',  # Column to display as hover text
-                    color_continuous_scale='Blues',  # Color scale
+                    range_color=[0, 100], 
+                    color_continuous_scale='RdYlGn',  # Color scale ranging from red to green
                     title='Data by Country')
 
 fig.update_geos(
@@ -50,14 +55,16 @@ fig.update_layout(
 df_mortality = pd.read_pickle('mortality_data.pkl')
 
 df_mortality = df_mortality[(df_mortality["DIM_TIME"] == 2019) & (df_mortality["DIM_SEX"] == "TOTAL")]
+df_mortality["log_value"] = np.log10(df_mortality["RATE_PER_100000_N"])
 
 # Create a choropleth map using Plotly
 fig_2 = px.choropleth(df_mortality, 
                     locations='GEO_NAME_SHORT',  # This should be the country name or ISO code
                     locationmode='country names',  # or 'ISO-3' if using 3-letter country codes
-                    color='RATE_PER_100000_N',  # The data column you want to visualize
+                    color='log_value',  # The data column you want to visualize
                     hover_name='GEO_NAME_SHORT',  # Column to display as hover text
-                    color_continuous_scale='Blues',  # Color scale
+                    hover_data='RATE_PER_100000_N', 
+                    color_continuous_scale='RdYlGn_r',  # Color scale (reversed)
                     title='Data by Country')
 
 fig_2.update_geos(
@@ -77,6 +84,11 @@ fig_2.update_layout(
     )
 )
 
+fig_2.update_coloraxes(
+    colorbar_tickvals=np.log10([1, 10, 100, 1000, 10000]),  # Tick locations
+    colorbar_ticktext=["1", "10", "100", "1k", "10k"],       # Original values
+)
+
 df_expectancy = pd.read_pickle('life_expectancy.pkl')
 df_expectancy['Year'] = df_expectancy['Year'].astype(int)
 df_expectancy['Value'] = df_expectancy['Value'].replace('..', pd.NA)
@@ -90,7 +102,8 @@ fig_3 = px.choropleth(df_expectancy,
                     locationmode='country names',  # or 'ISO-3' if using 3-letter country codes
                     color='Value',  # The data column you want to visualize
                     hover_name='Country Name',  # Column to display as hover text
-                    color_continuous_scale='Blues',  # Color scale
+                    color_continuous_scale='RdYlGn',  # Color scale
+                    range_color=[40, 85],  # Range of the color scale
                     title='Data by Country')
 
 fig_3.update_geos(
@@ -113,7 +126,41 @@ fig_3.update_layout(
 if df_expectancy.empty:
     st.write("No data available for the selected year.")
 
+df_wb_water = pd.read_pickle('wb_water_access.pkl')
+df_wb_water['Year'] = df_wb_water['Year'].astype(int)
+df_wb_water['Value'] = df_wb_water['Value'].replace('..', pd.NA)
+df_wb_water['Value'] = pd.to_numeric(df_wb_water['Value'], errors='coerce')
+
+df_wb_water = df_wb_water[(df_wb_water["Area"] == option) & (df_wb_water["Year"] == number)]
+
+# Create a choropleth map using Plotly
+fig_4 = px.choropleth(df_wb_water, 
+                    locations='Country Name',  # This should be the country name or ISO code
+                    locationmode='country names',  # or 'ISO-3' if using 3-letter country codes
+                    color='Value',  # The data column you want to visualize
+                    hover_name='Country Name',  # Column to display as hover text
+                    range_color=[0, 100], 
+                    color_continuous_scale='RdYlGn',  # Color scale ranging from red to green
+                    title='Data by Country')
+
+fig_4.update_geos(
+        showcoastlines=True, 
+        coastlinecolor="Black", 
+        showland=True, 
+        landcolor="lightgray",
+        center=dict(lat=2, lon=53),  # Set the center of the map (latitude, longitude)
+        projection_scale=2.3  # Adjust the zoom level (lower values zoom in more)
+    )
+fig_4.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+fig_4.update_layout(
+    title='People using at least basic drinking water services',
+    coloraxis_colorbar=dict(
+        title="Proportion of access (%)"  # Custom label
+    )
+)
+
 #st.plotly_chart(fig_2, use_container_width=True)
 st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig_4, use_container_width=True)
 st.plotly_chart(fig_2, use_container_width=True)
 st.plotly_chart(fig_3, use_container_width=True)
